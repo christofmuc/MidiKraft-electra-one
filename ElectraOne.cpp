5f0174134aedf80385a12b30813b95b0bd1f9322
Christof Ruch <christof.ruch@gmx.de>
2021-05-03 13:34:15 +0200

    First hackish implementation of code that perspectively should generate Electra One Presets and Instrument Definition files

---
+++ ElectraOne.cpp
@@ -0,0 +1,203 @@
+/*
+   Copyright (c) 2021 Christof Ruch. All rights reserved.
+
+   Dual licensed: Distributed under Affero GPL license by default, an MIT license is available for purchase
+*/
+
+#include "ElectraOne.h"
+
+#include "Capability.h"
+#include "MidiLocationCapability.h"
+
+#include "BinaryResources.h"
+
+#include <nlohmann/json-schema.hpp>
+
+namespace midikraft {
+
+	std::map<ElectraOneColor, std::string> kElectraOneColorValues = {
+		{ElectraOneColor::white, "FFFFFF"},
+		{ElectraOneColor::red, "F45C51 "},
+		{ElectraOneColor::orange, "F49500"},
+		{ElectraOneColor::blue, "529DEC"},
+		{ElectraOneColor::green, "03A598"},
+		{ElectraOneColor::pink, "C44795"},
+	};
+
+	std::string ElectraOneParameter::name() const
+	{
+		return name_;
+	}
+
+	ElectraOneControlType ElectraOneParameter::controlType() const
+	{
+		return controlType_;
+	}
+
+	ElectraOneInstrumentDefinition::ElectraOneInstrumentDefinition() 
+	{
+	}
+
+	void ElectraOneInstrumentDefinition::addControllers(std::vector<std::shared_ptr<ElectraOneParameter>> const &controllers)
+	{
+		controllers_ = controllers;
+	}
+
+	std::string ElectraOneInstrumentDefinition::createJson(std::shared_ptr<Synth> synth)
+	{
+		// Check if the synth has a channel we need to create the value change messages later
+		MidiChannel synthChannel = MidiChannel::fromZeroBase(0);
+		auto location = Capability::hasCapability<MidiLocationCapability>(synth);
+		if (location) {
+			synthChannel = location->channel();
+		}
+
+		// Start creating the JSON data
+		nlohmann::ordered_json instrument_definition;
+
+		instrument_definition["id"] = "testid";
+		instrument_definition["name"] = "Korg DW8000";
+		instrument_definition["manufacturer"] = "Korg";
+		instrument_definition["manufacturerId"] = "Korg";
+
+		instrument_definition["categories"] = nlohmann::json::array({ { "id", "global" }, { "label", "Globals" } });
+
+		instrument_definition["overlays"] = nlohmann::json::array();
+
+		instrument_definition["parameters"] = nlohmann::json::array();
+		
+		for (auto const &controller : controllers_) {
+			nlohmann::ordered_json parameter;
+			parameter["categoryId"] = "global";
+			parameter["type"] = "fader";
+			parameter["name"] = controller->name();
+			parameter["values"] = ElectraOnePreset::createValues(controller->param(), synthChannel);
+			instrument_definition["parameters"].push_back(parameter);
+		}
+
+		// Now, validate that we produced a valid Electra instrument definition file
+		nlohmann::json_schema::json_validator validator;
+		bool canValidate = true;
+		try {
+			std::string eif_schema(instr_json, instr_json + instr_json_size);
+			validator.set_root_schema(nlohmann::json::parse(eif_schema));
+		}
+		catch (const std::exception &e) {
+			SimpleLogger::instance()->postMessage("Validation of schema failed: " + String(e.what()));
+			canValidate = false;
+		}
+
+		if (canValidate) {
+			try {
+				validator.validate(instrument_definition);
+			}
+			catch (const std::exception &e) {
+				SimpleLogger::instance()->postMessage("Program error, invalid Electra Instrument Definition json created: " + String(e.what()));
+				return "";
+			}
+		}
+
+		return instrument_definition.dump();
+	}
+
+	ElectraOnePreset::ElectraOnePreset(std::string const &name, std::string const &id) : name_(name), id_(id)
+	{
+	}
+
+	void ElectraOnePreset::addControllers(std::vector<std::shared_ptr<ElectraOneParameter>>const &controllers)
+	{
+		controllers_ = controllers;
+	}
+
+	std::vector<std::shared_ptr<midikraft::ElectraOneParameter>> ElectraOnePreset::controllers()
+	{
+		return controllers_;
+	}
+
+	std::string ElectraOnePreset::createJson(MidiChannel channel)
+	{
+		nlohmann::ordered_json preset;
+		preset["version"] = 2;
+		preset["name"] = name_;
+		preset["projectId"] = id_;
+		preset["pages"] = nlohmann::json::array();
+		preset["devices"] = nlohmann::json::array({ { "id", 1 }, { "name", name_ },  { "instrumentID", id_ }, { "port", 1 }, { "channel", channel.toOneBasedInt() } });
+		preset["overlays"] = nlohmann::json::array();
+		preset["groups"] = nlohmann::json::array();
+		preset["controls"] = nlohmann::json::array();
+		createControllers(preset);
+
+		return preset.dump(4);
+	}
+
+	void ElectraOnePreset::createControllers(nlohmann::ordered_json &preset)
+	{
+		int xgrid = 170;
+		int ygrid = 88;
+		int controlID = 1;
+		for (auto const &controller : controllers_) {
+			nlohmann::ordered_json control;
+			jassert(controlID >= 1 && controlID <= 432);
+			control["id"] = controlID++;
+			control["name"] = controller->name();
+			control["color"] = kElectraOneColorValues[controller->color()];
+			control["pageId"] = 1;
+			control["controlSetId"] = 1;
+			control["type"] = "fader";
+			control["bounds"] = { 0 * xgrid, 0 * ygrid, 146, 56 };
+			control["values"] = nlohmann::ordered_json::array();
+
+			// Create the MidiMessage that changes this value
+			nlohmann::json controllerMsg;
+			controllerMsg["deviceId"] = 1;
+
+			auto values = createValues(controller->param(), MidiChannel::fromZeroBase(0));
+			control["values"] = values;
+			control["inputs"] = nlohmann::json::array();
+			control["inputs"].push_back({ {"podId", controller->encoderNumber() }, { "valueId", values[0]["id"]} });
+
+			// Add this to our list of controls
+			preset["controls"].push_back(control);
+		}
+	}
+
+	nlohmann::ordered_json ElectraOnePreset::createValues(std::shared_ptr<SynthParameterDefinition> param, MidiChannel channel)
+	{
+		nlohmann::ordered_json result = nlohmann::ordered_json::array();
+
+		switch (param->type()) {
+		case SynthParameterDefinition::ParamType::INT: {
+			auto intParam = std::dynamic_pointer_cast<SynthIntParameterCapability>(param);
+			nlohmann::ordered_json value = {
+				{ "id", param->name() },
+				{"min", intParam->minValue() },
+				{"max", intParam->maxValue() },
+				{"defaultValue", 0}
+			};
+			nlohmann::ordered_json message = {
+				{ "deviceId", 1},
+				{ "type", "sysex" },
+				{ "parameterNumber", intParam->sysexIndex() },
+				{"min", intParam->minValue() },
+				{"max", intParam->maxValue() },
+				//TODO construction of this array needs to go into a new capability. And sysexIndex() is wrong, it is actually the parameter number
+				{"data", { 0xf0, 0x42, 0x30 | channel.toZeroBasedInt(), 0x03, 0x41, intParam->sysexIndex(), { { "type", "value" }}, 0xf7}}
+			};
+			value["message"] = message;
+
+			result.push_back(value);
+			break;
+		}
+		case SynthParameterDefinition::ParamType::INT_ARRAY:
+		case SynthParameterDefinition::ParamType::LOOKUP:
+		case SynthParameterDefinition::ParamType::LOOKUP_ARRAY:
+			// Not supported
+			//jassertfalse;
+			break;
+		}
+
+		return result;
+	}
+
+
+}
---
+++ ElectraOne.h
@@ -0,0 +1,87 @@
+/*
+   Copyright (c) 2021 Christof Ruch. All rights reserved.
+
+   Dual licensed: Distributed under Affero GPL license by default, an MIT license is available for purchase
+*/
+
+#pragma once
+
+#include "JuceHeader.h"
+
+#include "Synth.h"
+#include "SynthParameterDefinition.h"
+
+#undef snprintf
+#include "nlohmann/json.hpp"
+
+namespace midikraft {
+
+	// These are hard coded
+	enum class ElectraOneColor {
+		white,
+		red,
+		orange,
+		blue,
+		green,
+		pink
+	};
+
+	enum ElectraOneControlType {
+		ENCODER, // well, slider
+		BUTTON,  // well, touch
+	};
+
+	class ElectraOneParameter {
+	public:
+		ElectraOneParameter(ElectraOneControlType controlType, std::string const name, int physicalEncoderNumber) : controlType_(controlType), name_(name), encoderNumber_(physicalEncoderNumber) {
+			jassert(physicalEncoderNumber >= 0 && physicalEncoderNumber <= 12);
+		}
+
+		std::string name() const;
+		ElectraOneControlType controlType() const;;
+		int encoderNumber() const { return encoderNumber_; }
+
+		virtual ElectraOneColor color() const = 0;
+		virtual std::shared_ptr<SynthParameterDefinition> param() const = 0;
+
+	private:
+		ElectraOneControlType controlType_;
+		std::string name_;
+		int encoderNumber_;
+	};
+
+	class ElectraOneInstrumentDefinition {
+	public:
+		ElectraOneInstrumentDefinition();
+
+		void addControllers(std::vector<std::shared_ptr<ElectraOneParameter>> const &controllers);
+
+		std::string createJson(std::shared_ptr<Synth> synth);
+
+	private:
+		std::shared_ptr<Synth> synth_;
+		std::vector<std::shared_ptr<ElectraOneParameter>> controllers_;
+	};
+
+	class ElectraOnePreset {
+	public:
+		ElectraOnePreset(std::string const &name, std::string const &id);
+
+		void addControllers(std::vector<std::shared_ptr<ElectraOneParameter>> const &controllers);
+		std::vector<std::shared_ptr<ElectraOneParameter>> controllers();
+
+		std::string createJson(MidiChannel channel);
+
+		static nlohmann::ordered_json createValues(std::shared_ptr<SynthParameterDefinition> param, MidiChannel channel);
+
+	private:
+		void createControllers(nlohmann::ordered_json &preset);
+
+		std::string name_;
+		std::string id_;
+		std::vector<std::shared_ptr<ElectraOneParameter>> controllers_;
+	};
+
+
+}
+
